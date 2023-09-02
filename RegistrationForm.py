e46cc7b291f75096962ceda0beabfbb22f4bbde1
Robert Vari <mail.robertvari@gmail.com>
2023-09-02 16:55:57 +0200

    update

--- RegistrationForm.py
+++ RegistrationForm.py
@@ -1,10 +1,43 @@
 import sys
+from typing import Optional
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QApplication, QWidget
+from PySide6.QtWidgets import (
+    QApplication, QWidget, QLineEdit, 
+    QVBoxLayout, QHBoxLayout, QTextEdit, 
+    QPushButton
+)
 
 
 class RegistrationForm(QWidget):
-    pass
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Registration Form")
+        self.create_window_content()
+
+    def create_window_content(self):
+        # Create Vertical Box Layout and assigne a parent widget to it
+        main_layout = QVBoxLayout(self)
+
+        # create text inputs
+        self.first_name_field = QLineEdit()
+        self.last_name_field = QLineEdit()
+        self.address_field = QLineEdit()
+        self.phone_field = QLineEdit()
+        self.email_field = QLineEdit()
+
+        # create a multiline text input
+        self.about_field = QTextEdit()
+
+        self.save_button = QPushButton("Save Data")
+
+        # add text inputs do main_layout
+        main_layout.addWidget(self.first_name_field)
+        main_layout.addWidget(self.last_name_field)
+        main_layout.addWidget(self.address_field)
+        main_layout.addWidget(self.phone_field)
+        main_layout.addWidget(self.email_field)
+        main_layout.addWidget(self.about_field)
+        main_layout.addWidget(self.save_button)
 
 
 if __name__ == "__main__":
